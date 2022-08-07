4fd92183f74d2995f82854e68a9b783cc7c405cb
pramodreddypandiri <pramodreddypandiri010@gmail.com>
2022-08-07 20:30:01 +0530

    almost completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'Pale-blue': 'hsl(225, 100%, 94%)',
+        'Bright-blue': 'hsl(245, 75%, 52%)',
+        'Very pale-blue': 'hsl(225, 100%, 98%)',
+        'Desaturated-blue': 'hsl(224, 23%, 55%)',
+        'Dark-blue': 'hsl(223, 47%, 23%)',
+      },
+      
+    }
   },
   plugins: [],
 }
